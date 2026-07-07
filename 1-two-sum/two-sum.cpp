24fa9cc834b02679d47522d00b61189d0067f7d7
Anushka Sharma <as4918@srmist.edu.in>
2026-07-07 22:19:44 +0530

    Time: 34 ms (35.34%) | Memory: 14.2 MB (68.67%) - LeetSync

---
+++ 1-two-sum/two-sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
+        
+        int n = nums.size();
+        int i;
+        int j;
+
+        for(int i =0;i <n;i++)
+        {
+            for(int j = i+1; j<n;j++)
+            {
+                if(nums[i]+ nums[j] == target)
+                {
+                return{i,j};
+                }
+            }
+
+           
+        }
+
+        return{};
+    }
+};
+    
