ef04413d5a30cb1f7730bda8c892ba78b4a3873f
Anushka Sharma <sharma.anushkaa06@gmail.com>
2026-09-18 14:10:39 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (25.95%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.c
@@ -0,0 +1,31 @@
+bool isValid(char* s)
+{
+    char stack[10000];
+    int top = -1;
+
+    for (int i = 0; s[i] != '\0'; i++)
+    {
+        if (s[i] == '(' || s[i] == '[' || s[i] == '{')
+        {
+            stack[++top] = s[i];
+        }
+        else
+        {
+            if (top == -1)
+                return false;
+
+            char open = stack[top--];
+
+            if (s[i] == ')' && open != '(')
+                return false;
+
+            if (s[i] == ']' && open != '[')
+                return false;
+
+            if (s[i] == '}' && open != '{')
+                return false;
+        }
+    }
+
+    return top == -1;
+}
