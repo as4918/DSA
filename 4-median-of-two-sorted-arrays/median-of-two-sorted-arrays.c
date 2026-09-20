c257d60f8a72d47914c8f7f20810debc2d26682d
Anushka Sharma <sharma.anushkaa06@gmail.com>
2026-09-20 22:13:17 +0530

    Time: 5 ms (12.33%) | Memory: 12.4 MB (14.56%) - LeetSync

---
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.c
@@ -0,0 +1,37 @@
+double findMedianSortedArrays(int* nums1, int nums1Size,
+                              int* nums2, int nums2Size)
+{
+    int total = nums1Size + nums2Size;
+    int *arr = malloc(total * sizeof(int));
+
+    int i = 0, j = 0, k = 0;
+
+    while (i < nums1Size && j < nums2Size)
+    {
+        if (nums1[i] < nums2[j])
+            arr[k++] = nums1[i++];
+        else
+            arr[k++] = nums2[j++];
+    }
+
+    while (i < nums1Size)
+        arr[k++] = nums1[i++];
+
+    while (j < nums2Size)
+        arr[k++] = nums2[j++];
+
+    double median;
+
+    if (total % 2 == 1)
+    {
+        median = arr[total / 2];
+    }
+    else
+    {
+        median = (arr[total / 2 - 1] + arr[total / 2]) / 2.0;
+    }
+
+    free(arr);
+
+    return median;
+}
